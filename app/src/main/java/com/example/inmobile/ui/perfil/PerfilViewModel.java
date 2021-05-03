955f8201322234725082c16189fb983519dc4cc8
Marcela Angella <43481279+marceFa@users.noreply.github.com>
2021-05-02 23:51:00 -0300

    cambio perfil

--- app/src/main/java/com/example/inmobile/ui/perfil/PerfilViewModel.java
+++ app/src/main/java/com/example/inmobile/ui/perfil/PerfilViewModel.java
@@ -10,15 +10,27 @@ import com.example.inmobile.request.ApiClient;
 public class PerfilViewModel extends ViewModel {
 
     private MutableLiveData<Propietario> propietarioMutable;
+    private MutableLiveData<String> mensajeMutable;
+    ApiClient apiClient= ApiClient.getApi();
+
     public PerfilViewModel() {
         super();
     }
+
     public LiveData<Propietario> getPropietarioMutable() {
         if (propietarioMutable == null) {
             propietarioMutable = new MutableLiveData<>();
         }
         return propietarioMutable;
     }
+
+    public LiveData<String> getMensajeMutable() {
+        if (mensajeMutable == null) {
+            mensajeMutable = new MutableLiveData<>();
+        }
+        return mensajeMutable;
+    }
+
     //este metodo nos trae al usuario Logueado
     public void obtenerDatos(){
 
@@ -30,10 +42,22 @@ public class PerfilViewModel extends ViewModel {
 
     public void editar(int id,Long dni ,String nombre, String apellido, String email,  String password, String tel){
 
-        ApiClient apiClient= ApiClient.getApi();
-        Propietario propietario = new Propietario( id, dni,nombre, apellido, email,password,tel);
-        apiClient.actualizarPerfil(propietario);
-        propietarioMutable.setValue(propietario);
+        Propietario propietario = new Propietario(id, dni, nombre, apellido, email, password, tel);
+
+
+        if( (dni != null) && (nombre.length()>0) && (nombre != null) && (apellido != null) && (apellido.length()>0) &&
+                ( email != null) && ( email.length() >0 ) && (password != null) &&
+                (password.length() > 0) && (tel != null ) && (tel.length()>0))
+        {
+            apiClient.actualizarPerfil(propietario);
+            propietarioMutable.setValue(propietario);
+            mensajeMutable.setValue("Sus datos fueron editados exitosamente!!!");
+
+        }
+        else
+        {
+            mensajeMutable.setValue("Verifique que ningún campo este vacio!!");
+        }
     }
 
 }
